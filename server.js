66fe4fccd9bd24401f13cdac84772dc2fcd0631c
Arthur Drumond <adbsoares@sga.pucminas.br>
2025-07-16 17:33:46 -0300

    Naming

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const agentesRouter = require("./routes/agentesRouter")
-const casosRouter = require("./routes/casosRouter")
+const agentesRouter = require("./routes/agentesRoutes")
+const casosRouter = require("./routes/casosRoutes")
 
 const app = express();
 const PORT = 3000;
