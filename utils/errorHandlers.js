2dd6ff7feae2da8c9823421aefb82e3c05094fb7
Arthur Drumond <adbsoares@sga.pucminas.br>
2025-07-16 11:30:15 -0300

    Tratamento de erro de corpo de requisição

--- utils/errorHandlers.js
+++ utils/errorHandlers.js
@@ -1,8 +1,32 @@
-// Função para validar request body, --> boolean
-function validate() {
+function validateFields(requestBody, expectedFields) {
+    // Illegal request body data type
+    if(typeof requestBody != "object") return false
 
+    // Invalid format date
+    if(requestBody.hasOwnProperty("dataDeIncorporacao")) {
+        const date = requestBody["dataDeIncorporacao"]
+        const regex = /^\d{4}-\d{2}-\d{2}$/;
+        return regex.test(date);
+    }
+
+
+    // Invalid fields
+    for (const key of expectedFields) { 
+        if (!requestBody.hasOwnProperty(key)) {
+            return false
+        }
+    }
+    return true
 }
 
+function isSubset(subsetArray, mainArray) {
+    if (subsetArray.length > mainArray.length) {
+      return false
+    }
+  
+    return subsetArray.every(element => mainArray.includes(element))
+  }
 module.exports = {
-    validate
+    validateFields,
+    isSubset
 }
